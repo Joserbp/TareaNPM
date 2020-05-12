c5fac605a987b152934238ec5a16743ee4362449
Joserbp <josera.jack@gmail.com>
2020-05-11 20:28:40 -0500

    Express y npm terminado

---
+++ index.js
@@ -0,0 +1,9 @@
+"use strict";
+  const express = require('express');
+  const app = express();
+
+  app.get('/', function (req, res) {
+  })
+
+  app.listen(3000);
+  console.log("Mi primer server corriendo en http://localhost:3000")
